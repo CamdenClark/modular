55248de542fd5e636f6526f09e47b3d5c2286764
Camden <camdenaws@gmail.com>
2021-10-10 20:52:25 -0700

    Added layout for mining page and crafting page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 import WalletContext from "./WalletContext";
 import Header from "./Header";
+import Craft from "./Craft";
+import Mine from "./Mine";
 
 export const App: FC = () => {
   return (
     <WalletContext>
       <Header />
+      <Craft />
+      <Mine />
     </WalletContext>
   );
 };
---
+++ frontend/src/Mine.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { FC } from "react";
+
+const MineInventory = () => {
+  console.log("Mining!");
+};
+
+export const Mine: FC = () => {
+  return (
+    <Grid container direction="row" justifyContent="center">
+      <Grid
+        container
+        item
+        xs={10}
+        md={8}
+        direction="row"
+        justifyContent="flexStart"
+        alignItems="center"
+        style={{ marginTop: "5rem" }}
+      >
+        <Typography variant="h1">Mine an inventory to get started!</Typography>
+
+        <Button
+          color="primary"
+          variant="outlined"
+          size="large"
+          onClick={MineInventory}
+          style={{ marginTop: "3rem", padding: "2rem 5rem", fontSize: "2rem" }}
+        >
+          Mine
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Mine;
